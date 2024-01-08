ee976fd062b8283854c7a8d1aed56275eaca5d72
ALina Shewczowa <shewczowa.alina@yandex.ru>
2024-01-08 05:05:03 +0300

    1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import org.example.Server;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int poolSize = 64;
+        int port = 9999;
+        Server server = new Server(poolSize);
+        server.start(port);
+    }
+}
